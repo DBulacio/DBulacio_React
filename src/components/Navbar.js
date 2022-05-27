3215bef1ce085a537ffa8054e84e9a7882fcfd82
DBulacio <daniel.bulacio@hotmail.com>
2022-05-27 14:54:09 -0300

    started new web with react!

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar() {
+    return(
+        <nav>
+            <ul>
+                <li>ABOUT</li>
+                <li>TL;DR</li>
+                <li>PROJECTS</li>
+            </ul>
+        </nav>
+    )
+}
